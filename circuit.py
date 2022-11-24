195ccb325a271812332280692535eb3c5d1e7fe5
Yakup Çatalkaya <31369965+yakupcatalkaya@users.noreply.github.com>
2022-11-24 23:11:35 +0300

    Update circuit.py

--- circuit.py
+++ circuit.py
@@ -5,7 +5,6 @@ Created on Wed Oct  5 18:59:16 2022
 @author: yakupcatalkaya
 """
 
-# import math
 from matplotlib import pyplot as plt
 from scipy.integrate import odeint
 import numpy as np
@@ -20,18 +19,14 @@ class Item():
     
     """
     def __init__(self, resistance=None, name=None):
-        global freq
+        global freq, time
         
         self.resistance = resistance        
         self.name = name
         self.input_v = 0
         self.potential = 1
         self.period = 1 / freq
-
-    
-    def update_values(self, value_names, new_values):
-        for value_name, new_value in value_names, new_values:
-            setattr(self, value_name, new_value)
+        self.time = time
         
 
 
@@ -42,85 +37,36 @@ class Resistor(Item):
     and membrane resistance
     
     """
-    def __init__(self, name, m=0, n=0, h=1, memb_pot=-65, current=0, leak=0,
-                 start_point=None, conductance=None):
+    def __init__(self, name, memb_pot=-65, current=0, leak=0,
+                 start_point=None, conductance=None, time=None):
         Item.__init__(self)
         self.name = name.lower().strip()
-        self.m_init, self.n_init, self.h_init = m, n, h
-        self.m, self.n, self.h = m, n, h
         self.memb_pot = memb_pot
-        self.u = memb_pot
         self.current = current
         self.conductance = conductance
-        self.constant_conductance = conductance
+        self.const_cond = conductance
         self.leak_potential = leak
-        self.input_v = start_point.value
-        self.output_v = self.input_v + self.potential + self.leak_potential
-        
-        
-    def ode_for_mhn(self, y0, t, mhn):
-        global v_intracellular_rest
-        
-        self.u = self.memb_pot - v_intracellular_rest
-        
-        if mhn == "m":
-            self.m_alpha = (0.1*(self.u-25))/(1-np.exp(-(self.u-25)/10))
-            self.m_beta = 4*np.exp(-self.u/18)
-            self.dm = self.m_alpha*(1-self.m)-self.m_beta*self.m
-            self.m_tau = 1 / (self.m_alpha + self.m_beta)
-            self.m_zero = y0
-            return self.dm
+        self.time = time
+        self.m = Vector(self.name + "_m")
+        self.h = Vector(self.name + "_h")
+        self.n = Vector(self.name + "_n")
+        self.cond_record = Vector(self.name + "_conductance")
+        self.i_record = Vector(self.name + "_current")
 
-        elif mhn == "n":
-            self.n_alpha = (0.01*(self.u-10))/(1-np.exp(-(self.u-10)/10))
-            self.n_beta = 0.125*np.exp(-self.u/80)
-            self.dn = self.n_alpha*(1-self.n)-self.n_beta*self.n
-            self.n_tau = 1 / (self.n_alpha + self.n_beta)
-            self.n_zero = y0
-            return self.dn
-            
-        elif mhn == "h":
-            self.h_alpha = 0.07*np.exp(-self.u/20)
-            self.h_beta = 1/(1+np.exp(-(self.u-30)/10))
-            self.dh = self.h_alpha*(1-self.h)-self.h_beta*self.h
-            self.h_tau = 1 / (self.h_alpha + self.h_beta)
-            self.h_zero = y0
-            return self.dh
-        
 
-    def update_variables(self, v_intracellular, v_extracellular, at_t,
-                         n_zero=0.01, m_zero=0.001, h_zero=0.9):
-        global time
-        
-        self.memb_pot = (v_intracellular.value - v_extracellular.value)
-        self.m_ode = odeint(self.ode_for_mhn, y0=m_zero, t=time, args=("m",))
-        self.h_ode = odeint(self.ode_for_mhn, y0=h_zero, t=time, args=("h",))
-        self.n_ode = odeint(self.ode_for_mhn, y0=n_zero, t=time, args=("n",))
-        
-        self.m_inf = self.m_ode[-1]
-        self.h_inf = self.h_ode[-1]
-        self.n_inf = self.n_ode[-1]
-        
-        self.m = self.m_zero-((self.m_zero-self.m_inf)*(1-np.exp(-at_t/self.m_tau)))
-        self.h = self.h_zero-((self.h_zero-self.h_inf)*(1-np.exp(-at_t/self.h_tau)))
-        self.n = self.n_zero-((self.n_zero-self.n_inf)*(1-np.exp(-at_t/self.n_tau)))
-        
-        if self.name == "g_na":
-            self.conductance = self.constant_conductance *self.h * self.m ** 3
-            self.current = self.conductance * (self.u - self.leak_potential)
-            
-        elif self.name == "g_k":
-            self.conductance = self.constant_conductance * self.n ** 4
-            self.current = self.conductance * (self.u - self.leak_potential)
-            
-        elif self.name == "g_cl":
-            self.conductance = self.constant_conductance
-            self.current = self.conductance * (self.u - self.leak_potential)
-            
-        else:
-            raise SystemError("Look at here.")
-        
-    
+    def update_variables(self):
+        for index, item in enumerate(self.time):
+            if self.name == "g_na":
+                self.cond_record.record(self.const_cond *self.h.val[index] * self.m.val[index] ** 3)
+            elif self.name == "g_k":
+                self.cond_record.record(self.const_cond * self.n.val[index] ** 4)
+            elif self.name == "g_cl":
+                self.cond_record.record(self.const_cond)
+            else:
+                raise SystemError("Look at here.")
+            # self.i_record.val = (self.cond_record.val * self.memb_pot)
+
+
     def inform(self):
         print("\n", self.name, "\n", "-" * 20)
         print(" h:", self.h, "\n", "m:", self.m, "\n", "n:", self.n)
@@ -160,6 +106,7 @@ class VoltageSource(Item):
         Item.__init__(self)
         self.name = name.lower().strip()
         self.potential = init_voltage
+        self.val = init_voltage
 
 
 
@@ -172,13 +119,28 @@ class CurrentSource(Item):
     that will start action potential.
     
     """
-    def __init__(self, init, name):
-        self.value = init
+    def __init__(self, initial, time, name, value):
+        self.time = time
+        self.initial = initial
+        self.value = value
         self.name = name.lower().strip()
-    
-    
-    def update(self, new_value):
-        self.value = new_value
+        self.values = np.zeros((len(self.time),))
+        for idx, item in enumerate(self.values): 
+            self.values[idx] = self.get_value(self.time[idx])
+
+            
+    def get_value(self, t):
+        if 10.0 < t < 10.5:
+            return self.value
+        return self.initial
+
+
+    def update(self, at_t=0, new=None):
+        if new==None:
+            self.value = self.values[at_t]
+        else:
+            self.value = new
+        return self.value
 
 
 
@@ -211,10 +173,9 @@ class Vector():
     def __init__(self, name):
         global freq, start_time, stop_time, time
         self.name = name.lower().strip()
-        self.val = np.zeros(int((stop_time - start_time) * freq))
         self.update_index = 0
         self.time = time
-
+        self.val = np.zeros((len(self.time),), dtype=object)
 
     def record(self, new_value):
         self.val[self.update_index] = new_value
@@ -227,92 +188,112 @@ class Vector():
     
     def plotter(self, labelx="", labely="", title="", label="", limx=None):
         plt.figure()
-        plt.plot(self.time, self.val, color='blue', label=label)
+        plt.plot(self.time[200:], self.val[200:], color='blue', label=label)
         plt.ylabel(labely)
         plt.xlabel(labelx)
         plt.title(title)
         plt.xlim(limx)
+        plt.savefig(title)
         plt.show()
     
 
+def initial(u=0.0):
+    m_alpha = (0.1*(u-25))/(1-np.exp(-(u-25)/10))
+    m_beta = 4*np.exp(-u/18)
 
-def ode_for_vm(y0, t, v_in_nominator, v_in_denominator, stimuli_i, memb_capacitance):
-    dvdt = ((v_in_nominator / v_in_denominator) + stimuli_i) / memb_capacitance
-    print(dvdt)
-    return dvdt
+    h_alpha = 0.07*np.exp(-u/20)
+    h_beta = 1/(1+np.exp(-(u-30)/10))
+    
+    n_alpha = (0.01*(u-10))/(1-np.exp(-(u-10)/10))
+    n_beta = 0.125*np.exp(-u/80)
+    
+    n_inf = n_alpha / (n_alpha + n_beta)
+    h_inf = h_alpha / (h_alpha + h_beta)
+    m_inf = m_alpha / (m_alpha + m_beta)
+    
+    return np.array([u, n_inf, h_inf, m_inf])
 
 
-freq = 40e3
-start_time = 0
-period = 1 / freq
-repeat = 1500
+def ode_solver(y, t):
+    u, n, m, h = y[0], y[1], y[2], y[3]
+    dy = np.zeros((4,))
+    
+    gk = (g_k.const_cond / memb_capacitance.value) * np.power(n, 4.0)
+    gna = (g_na.const_cond / memb_capacitance.value) * np.power(m, 3.0) * h
+    gcl = (g_cl.const_cond / memb_capacitance.value)
+    
+    m_alpha = (0.1 * (25.0 - u)) / (np.exp(2.5 - (0.1 * u)) - 1.0)
+    m_beta = 4.0 * np.exp(-u / 18.0)
+    h_alpha = 0.07 * np.exp(-u / 20.0)
+    h_beta = 1.0 / (np.exp(3.0 - (0.1 * u)) + 1.0)
+    n_alpha = (0.01 * (10.0 - u)) / (np.exp(1.0 - (0.1 * u)) - 1.0)
+    n_beta = 0.125 * np.exp(-u / 80.0)
+    
+    dv = stimuli.get_value(t) / memb_capacitance.value
+    dv -= gk * (u-e_k.val) + gna * (u-e_na.val) + gcl * (u-e_cl.val)
+    dm = m_alpha * (1 - m) - m_beta * m
+    dn = n_alpha * (1 - n) - n_beta * n
+    dh = h_alpha * (1 - h) - h_beta * h
+
+    dy[0], dy[1], dy[2], dy[3]= dv, dn, dm, dh
+
+    return dy
+
+
+freq = int(10e3)
+start_time = 0.0
 v_intracellular_rest = -65
-stop_time = repeat * period
-time = np.arange(start_time, stop_time, period, dtype=np.float32)
+stop_time = 20.0
+np.random.seed(1000)
+time = np.linspace(start_time, stop_time, freq)
 
 v_extracellular = VoltagePoint(0, "v_extracellular")
 v_intracellular = VoltagePoint(v_intracellular_rest, "v_intracellular")
+v_memb_potential = VoltagePoint(v_intracellular_rest,"v_memb")
 
-e_k = VoltageSource("e_k", -75)
-e_na = VoltageSource("e_na", +55)
-e_cl = VoltageSource("e_cl", -69)
+e_k = VoltageSource("e_k", +12)
+e_na = VoltageSource("e_na", -115)
+e_cl = VoltageSource("e_cl", -10.613)
 
-memb_capacitance = Capacitor(1, "Membrance_Capacitance")
-
-stimuli = CurrentSource(0, "Stimuli_current")
+memb_capacitance = Capacitor(1.0, "Membrance_Capacitance")
+stimuli = CurrentSource(0, time, "Stimuli_current", 200)
 
 g_k = Resistor(name="g_k", start_point=v_extracellular, 
-                leak=e_k.potential, conductance=35)
+                leak=e_k.potential, conductance=36.0, time=time)
 g_na = Resistor(name="g_na", start_point=v_extracellular, 
-                leak=e_na.potential, conductance=40)
+                leak=e_na.potential, conductance=120.0, time=time)
 g_cl = Resistor(name="g_cl", start_point=v_extracellular, 
-                leak=e_cl.potential, conductance=0.3)
+                leak=e_cl.potential, conductance=0.3, time=time)
    
-v_intra_record = Vector("v_intra")
-gk_record = Vector("gk_conductance") 
-gna_record = Vector("gna_conductance")
-gcl_record = Vector("gcl_conductance")
-gk_m_record = Vector("gk_m")
-gk_n_record = Vector("gk_n")
-gk_h_record = Vector("gk_h")
+v_memb_record = Vector("v_membrane")
+            
+solution = odeint(ode_solver, initial(), time).T
+new_v_memb, m, n, h = solution[0], solution[1], solution[2], solution[3]
+v_memb_record.val = new_v_memb + v_intracellular_rest
 
-for rep in range(repeat):
-    v_in_nominator = 0
-    v_in_denominator = 0
-    if rep == 1000: stimuli.update(1)
-    if rep == 1200: stimuli.update(0)
-    
-    for item in [g_k, g_na, g_cl]: 
-        item.update_variables(v_intracellular, v_extracellular, at_t=rep, 
-                              n_zero=item.n, m_zero=item.m, h_zero=item.h)
-        v_in_nominator += item.conductance * item.leak_potential
-        v_in_denominator += item.conductance
-        
-    # ode_for_vm(v_in_nominator, v_in_denominator, stimuli.value, memb_capacitance.value)
-    
-    new_v_intracellulars = odeint(ode_for_vm, v_intracellular.value, time, 
-                                 args=(v_in_nominator, v_in_denominator, 
-                                       stimuli.value, memb_capacitance.value))
-    new_v_intracellular = new_v_intracellulars[0]
-    v_intracellular.update_v(new_v_intracellular)
-    v_intra_record.record(v_intracellular.value)
-    gk_record.record(g_k.conductance)
-    gna_record.record(g_na.conductance)
-    gcl_record.record(g_cl.conductance)
-    gk_m_record.record(g_k.m)
-    gk_n_record.record(g_k.n)
-    gk_h_record.record(g_k.h)
+v_memb_record.plotter(labelx="time(ms)", labely="Membrance potential (mV)", 
+                       title="Action Potential Graph")
 
+m_record = Vector("m")
+h_record = Vector("h")
+n_record = Vector("n")
+m_record.val, h_record.val, n_record.val = m, h, n
 
-v_intra_record.plotter(labelx="time(s)", labely="Membrance potentiak(mV)", 
-                       title="Action Potential Graph", limx=(990 * period, 1100 * period))
+for item in [g_k, g_na, g_cl]:
+    item.m.val, item.n.val, item.h.val= m, n, h
 
-gk_record.plotter(labelx="time(s)", labely="Conductance(uS)", title="g_k graph")
-gna_record.plotter(labelx="time(s)", labely="Conductance(uS)", title="g_na graph")
-gcl_record.plotter(labelx="time(s)", labely="Conductance(uS)", title="g_cl graph")
+stimuli_record = Vector("stimuli")
+stimuli_record.val = stimuli.values
+stimuli_record.plotter(labelx="time(ms)", labely="Current (uA/cm2)", title="Stimuli graph")
 
-gk_m_record.plotter(labelx="time(s)", labely="m", title="m values")
-gk_n_record.plotter(labelx="time(s)", labely="n", title="n values")
-gk_h_record.plotter(labelx="time(s)", labely="h", title="h values")
+g_k.update_variables()
+g_na.update_variables()
+g_cl.update_variables()
 
-# plt.plot(v_intra_record.val[990:1010])
+g_k.cond_record.plotter(labelx="time(ms)", labely="Conductance(uS)", title="g_k graph")
+g_na.cond_record.plotter(labelx="time(ms)", labely="Conductance(uS)", title="g_na graph")
+g_cl.cond_record.plotter(labelx="time(ms)", labely="Conductance(uS)", title="g_cl graph")
+
+m_record.plotter(labelx="time(ms)", labely="m", title="m values")
+h_record.plotter(labelx="time(ms)", labely="h", title="h values")
+n_record.plotter(labelx="time(ms)", labely="n", title="n values")
